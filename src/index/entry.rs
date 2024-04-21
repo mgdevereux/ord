577a4abd0fb868cbc6816ad2c3f3e4a0b651ce49
mgdevereux <michael.devereux@gmail.com>
2024-04-21 20:25:57 +0100

    Update entry.rs

--- src/index/entry.rs
+++ src/index/entry.rs
@@ -63,7 +63,7 @@ impl RuneEntry {
     };
 
     if let Some(start) = self.start() {
-      if height < start {
+      if height <( start -1){
         return Err(MintError::Start(start));
       }
     }
